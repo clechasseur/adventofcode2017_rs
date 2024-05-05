c076dbf3c1fff998bbca2636a8d3c8c58d720103
Charles Lechasseur <shiftingbeard@gmx.com>
2024-05-04 22:25:21 -0400

    Day 2

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 //! Solutions to the Advent of Code 2017 puzzles in Rust 🦀
 
 pub mod day_1;
+pub mod day_2;
 pub(crate) mod input;
---
+++ tests/day_2.rs
@@ -0,0 +1,11 @@
+use adventofcode2017_clp::day_2::{part_1, part_2};
+
+#[test]
+fn day_2_part_1() {
+    assert_eq!(51139, part_1());
+}
+
+#[test]
+fn day_2_part_2() {
+    assert_eq!(272, part_2());
+}
