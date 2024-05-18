ccb2a9522095d0e09c77f28668f47e45a11ff1f9
Charles Lechasseur <shiftingbeard@gmx.com>
2024-05-18 12:51:56 -0400

    Day 4

--- src/lib.rs
+++ src/lib.rs
@@ -5,5 +5,6 @@
 pub mod day_1;
 pub mod day_2;
 pub mod day_3;
+pub mod day_4;
 pub(crate) mod helpers;
 pub(crate) mod input;
---
+++ tests/day_4.rs
@@ -0,0 +1,11 @@
+use adventofcode2017_clp::day_4::{part_1, part_2};
+
+#[test]
+fn day_4_part_1() {
+    assert_eq!(477, part_1());
+}
+
+#[test]
+fn day_4_part_2() {
+    assert_eq!(167, part_2());
+}
