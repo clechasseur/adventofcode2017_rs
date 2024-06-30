ee002128b954ccfbecab8c3b51470c306f4460a8
Charles Lechasseur <shiftingbeard@outlook.com>
2024-06-29 21:29:21 -0400

    Day 10

---
+++ tests/d10/mod.rs
@@ -0,0 +1,11 @@
+use adventofcode2017_clp::day_10::{part_1, part_2};
+
+#[test]
+fn day_10_part_1() {
+    assert_eq!(62_238, part_1());
+}
+
+#[test]
+fn day_10_part_2() {
+    assert_eq!("2b0c9cc0449507a0db3babd57ad9e8d8", part_2());
+}
