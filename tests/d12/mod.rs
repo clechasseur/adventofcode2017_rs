0d45cb07fbb21b4f01564247206aaac4f3df4bad
Charles Lechasseur <shiftingbeard@outlook.com>
2024-07-12 23:32:39 -0400

    Day 12

---
+++ tests/d12/mod.rs
@@ -0,0 +1,11 @@
+use adventofcode2017_clp::day_12::{part_1, part_2};
+
+#[test]
+fn day_12_part_1() {
+    assert_eq!(306, part_1());
+}
+
+#[test]
+fn day_12_part_2() {
+    assert_eq!(200, part_2());
+}
