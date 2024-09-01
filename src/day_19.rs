889a3dd82d3b5a345a3b489ccb74ec88954c5697
Charles Lechasseur <shiftingbeard@outlook.com>
2024-08-31 21:45:53 -0400

    Simplify day 19: we can't go out of bounds

--- src/day_19.rs
+++ src/day_19.rs
@@ -29,12 +29,6 @@ fn packet() -> impl Iterator<Item = (Turtle<i64>, Option<u8>)> {
     let start_pos = Pt::new(start_x as i64, 0);
     let turtle = Turtle::new(start_pos, Direction::Down);
 
-    let end_x = input[0].len() as i64;
-    let end_y = -(input.len() as i64);
-    let valid = move |t: &Turtle<i64>| {
-        let pos = t.position();
-        pos.x >= 0 && pos.x < end_x && pos.y <= 0 && pos.y > end_y
-    };
     let at = move |t: &Turtle<i64>| {
         let pos = t.position();
         input[(-pos.y) as usize].as_bytes()[pos.x as usize]
@@ -42,18 +36,16 @@ fn packet() -> impl Iterator<Item = (Turtle<i64>, Option<u8>)> {
 
     successors(Some((turtle, None)), move |(turtle, _)| {
         Some(turtle.advance())
-            .filter(valid)
             .filter(|t| at(t) != b' ')
             .or_else(|| {
                 Some(turtle.turn_left().advance())
-                    .filter(valid)
                     .filter(|t| {
+                        // Note: if I remove the test for `blockade`, it still works with my data.
                         let new_c = at(t);
                         new_c != b' ' && new_c != blockade(t.direction())
                     })
                     .or_else(|| Some(turtle.turn_right().advance()))
             })
-            .filter(valid)
             .map(|t| (t, at(&t)))
             .filter(|&(_, c)| c != b' ')
             .map(|(t, c)| (t, Some(c).filter(u8::is_ascii_uppercase)))
