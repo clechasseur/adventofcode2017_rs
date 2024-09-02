085672549afcf9da16b9bfe1aa72be7ad8ab55e9
Charles Lechasseur <shiftingbeard@outlook.com>
2024-09-01 22:00:49 -0400

    Refactor test files structure

---
+++ tests/days/day_01.rs
@@ -0,0 +1,11 @@
+use adventofcode2017_clp::day_01::{part_1, part_2};
+
+#[test]
+fn day_01_part_1() {
+    assert_eq!(1228, part_1());
+}
+
+#[test]
+fn day_01_part_2() {
+    assert_eq!(1238, part_2());
+}
