7b5d1d431da19d83dd68d99d6713b2dc30eb9534
Charles Lechasseur <shiftingbeard@outlook.com>
2024-09-24 22:37:40 -0400

    Optimize day 22 by using a fixed-size cluster

--- tests/days/day_22.rs
+++ tests/days/day_22.rs
@@ -6,7 +6,6 @@ fn day_22_part_1() {
 }
 
 #[test]
-#[cfg_attr(not(feature = "slow"), ignore = "runs in ~6s, use `--features slow` to enable")]
 fn day_22_part_2() {
     assert_eq!(2_511_416, part_2());
 }
