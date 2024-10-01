cc76eb3cfa310fe1d7144f0f1ddffe902bdac742
Charles Lechasseur <shiftingbeard@outlook.com>
2024-10-01 16:44:32 -0400

    Day 24

--- src/lib.rs
+++ src/lib.rs
@@ -25,5 +25,6 @@ pub mod day_20;
 pub mod day_21;
 pub mod day_22;
 pub mod day_23;
+pub mod day_24;
 pub(crate) mod helpers;
 pub(crate) mod input;
---
+++ tests/days/day_24.rs
@@ -0,0 +1,11 @@
+use adventofcode2017_clp::day_24::{part_1, part_2};
+
+#[test]
+fn day_24_part_1() {
+    assert_eq!(2_006, part_1());
+}
+
+#[test]
+fn day_24_part_2() {
+    assert_eq!(1_994, part_2());
+}
